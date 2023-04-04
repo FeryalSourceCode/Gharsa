b1ac9b18809a907ebf7ad235951f1e84960879f2
LailaAbd <107801922+LailaAbd@users.noreply.github.com>
2023-04-04 21:23:09 +0300

    Update text_form_global.dart

--- lib/components/text_form_global.dart
+++ lib/components/text_form_global.dart
@@ -1,32 +1,43 @@
 import 'package:flutter/material.dart';
 
 class TextFormGlobal extends StatelessWidget {
-  const TextFormGlobal({Key? key, required this.controller}) : super(key: key);
+  const TextFormGlobal({
+    Key? key,
+    required this.controller,required this.icon,required this.text,
+  }) : super(key: key);
   final TextEditingController controller;
+  final Icon icon;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 55,
-      padding: const EdgeInsets.only(top: 3, left: 15),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(6),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 7,
-            )
-          ]),
-      child: TextFormField(
-        controller: controller,
-        keyboardType: TextInputType.emailAddress,
-        decoration: const InputDecoration(
-            hintText: "Email",
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.all(0),
-            hintStyle: TextStyle(height: 1)),
+    return Container(margin: EdgeInsets.all(10),
+
+      child: SizedBox(
+        width: 300,
+
+
+
+        child: TextField(
+
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(top: 10),
+            filled: true,
+              fillColor: Colors.black12,
+
+              prefixIcon: icon,
+              hintText: text,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(width:2,color: Colors.red),
+              )),
+        ),
       ),
     );
   }
 }
+
